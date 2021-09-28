6463e6e103ece081647cab810489779f2fd5ed0d
Sunny-jinn <rlawlsdn316@gmail.com>
2021-09-29 00:36:02 +0900

    Complete Nweeting

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "../fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, serUserObj] = useState(null);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        serUserObj(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "Initialzing..."
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
+import { Redirect } from "react-router";
+
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
